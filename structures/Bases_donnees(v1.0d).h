791bd5883e2003425104b7b56daac089dbee0ba9
Liam <liam.brocvielle@isen-lille.fr>
2018-01-09 13:41:21 +0100

    Fichier structures modifies

--- structures/Bases_donnees(v1.0d).h
+++ structures/Bases_donnees(v1.0d).h
@@ -1,4 +1,5 @@
 #pragma once
+#define CONSTANTE_A_CHANGER 100
 typedef struct coord {//coordonnées d'un lieu (latitude, longitude)
 
 	int x; // latitude
@@ -39,9 +40,9 @@ typedef struct article {//caractéristiques d'un article
 typedef struct entrepot {//caractéristiques de l'entrepôt + inventaire
 
 	int Id;//numéro de l'entrepôt
-	stock tab_stock[100];//inventaire (max fixé à 100 articles différents)
+	stock tab_stock[CONSTANTE_A_CHANGER];//inventaire (max fixé à 100 articles différents)
 	coord coordonnees;//coordonnées de l'entrepôt
-	livraison tab_de_livraison[100];
+	livraison tab_livraisons[CONSTANTE_A_CHANGER];//livraisons devant être effectuées par l'entrepôt
 }entrepot;
 
 typedef struct client {//informations d'un client
@@ -63,13 +64,13 @@ typedef struct commande {
 typedef struct  stock {
 
 	int Id_article;//identifiant de l'article
-	int Nb;//nombre d'articles restant dans un entrepot
+	int Nb;//nombre d'articles restant dans un entrepot ou nombre d'articles commandés
 }stock;
 
 typedef struct slot {//caractéristiques d'un casier d'une pickup station
 
 	dim taille;//dimensions du casier
-	int dispo;// disponibilité du casier: 0=occupe, 1=disponible
+	int dispo;// disponibilité du casier: 0=disponible, 1=occupé
 	int code; //code correspondant à la commande
 }slot;
 
@@ -78,13 +79,14 @@ typedef struct Pickup {//carctéristiques d'une pickup station
 	int Id;// identifiant de la pickup station
 	int Nb_places;//nombre de casiers disponibles dans la station
 	coord coordonnees;//coodonnees de la station
+	adresse adresse; //adresse de la station
 	slot tab_slot[]; // tableau contenant les caractéristiques de tous les casiers de la station
+	
 }PickupStation;
 
-typedef struct livre{
+typedef struct livr {
 
 	adresse adresse_pickup;//adresse de livraison
 	commande commande;//commande du client
 
 }livraison;
-
